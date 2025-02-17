1cc01732d38db3ab89cfee25a8a6cff57fcad341
cialfin <financializer@gmail.com>
2025-02-17 07:51:46 -0500

    Adding Pages

--- search/script.js
+++ search/script.js
@@ -1,16 +1,29 @@
  $(document).ready(function() {
- 	// Function to get query parameters
- 	function getQueryParam(param) {
- 		const urlParams = new URLSearchParams(window.location.search);
- 		return urlParams.get(param);
- 	}
+	// Function to get query parameters
+	function getQueryParam(param) {
+		const urlParams = new URLSearchParams(window.location.search);
+		return urlParams.get(param);
+	}
 
- 	// Get the value of the 'q' parameter
- 	const queryValue = getQueryParam('q');
+	// Get the value of the 'q' parameter
+	const queryValue = getQueryParam('q');
 
- 	// If the 'q' parameter exists and has a value
- 	if (queryValue) {
- 		// Prepend the unencoded value to the document title
- 		document.title = decodeURIComponent((queryValue.charAt(0).toUpperCase() + queryValue.slice(1))) + ' - ' + document.title;
- 	}
+	// If the 'q' parameter exists and has a value
+	if (queryValue) {
+		// Prepend the unencoded value to the document title
+		document.title = decodeURIComponent((queryValue.charAt(0).toUpperCase() + queryValue.slice(1))) + ' - ' + document.title;
+	}
+
+	// 
+	// 
+
+	var checkExist = setInterval(function() {
+		if ($('#gsc-i-id1').length) {
+			$('#gsc-i-id1').focus();
+			clearInterval(checkExist); // Stop checking once the element is found
+		}
+	}, 100); // Check every 100 milliseconds
+
+	// 
+	// 
  });
